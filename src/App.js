3d5d25a890f51fabdd58ed7158e542dae0dfe2b7
Juan <juanan0309@gmail.com>
2021-09-13 14:35:04 -0500

    star wars api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [info, setInfo] = useState({});
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people").then((r) => setInfo(r.data));
+  }, []);
+
+  const handleNext = () => {
+    axios.get(info.next).then((r) => setInfo(r.data));
+  };
+
+  const handlePrevious = () => {
+    axios.get(info.previous).then((r) => setInfo(r.data));
+  };
+
+  let nextDisable = info.next === null ? true : false;
+  let previousDisable = info.previous === null ? true : false;
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Star Wars API</h1>
       </header>
+      <section className="results">
+        <ul className="list">
+          {!!info.results &&
+            info.results.map((item) => (
+              <li key={item.name}>
+                <p>name: {item.name}</p>
+                <p>height: {item.height}</p>
+                <p>mass: {item.mass}</p>
+                <p>hair_color: {item.hair_color}</p>
+                <p>skin_color: {item.skin_color}</p>
+                <p>eye color: {item.eye_color}</p>
+                <p>birth year: {item.birth_year}</p>
+                <p>gender: {item.gender}</p>
+              </li>
+            ))}
+        </ul>
+      </section>
+      <section className="buttons">
+        <button disabled={previousDisable} onClick={handlePrevious}>
+          Previous
+        </button>
+        <button disabled={nextDisable} onClick={handleNext}>
+          Next
+        </button>
+      </section>
     </div>
   );
 }
